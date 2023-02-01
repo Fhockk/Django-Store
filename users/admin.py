b23f3821c3959685926d50fd0f13bf57881c1342
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-02-01 20:57:12 +0200

    pagination + admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 from users.models import User
+from products.admin import BasketAdmin
 
-admin.site.register(User)
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username',)
+    inlines = (BasketAdmin,)
