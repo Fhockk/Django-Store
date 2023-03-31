d363f9da6be4c5bd8af8fa747ff5c38aff76cb27
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-03-31 14:07:21 +0300

    create orders with relevant data

--- orders/forms.py
+++ orders/forms.py
@@ -4,6 +4,27 @@ from orders.models import Order
 
 
 class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'John'
+    }))
+
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Smith'
+    }))
+
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'example@mail.com'
+    }))
+
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Kharkiv'
+    }))
+
+
     class Meta:
         model = Order
         fields = ('first_name', 'last_name', 'email', 'address')
