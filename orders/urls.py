b79345b66184aed4f949ca58de8fddc0eca587ce
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-04-04 17:26:14 +0300

    stipe configuration (test-mode)

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from orders.views import OrderCreateView
+from orders.views import OrderCreateView, CancelTemplateView, SuccessTemplateView, OrderListView, OrderDetailView
 
 app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
+    path('', OrderListView.as_view(), name='orders_list'),
+    path('order/<int:pk>', OrderDetailView.as_view(), name='order'),
+    path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
+    path('order-cancelled/', CancelTemplateView.as_view(), name='order_cancelled'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,50 @@
+import stripe
+
+from http import HTTPStatus
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy, reverse
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
+from products.models import Basket
+from orders.models import Order
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class SuccessTemplateView(TitleMixin, TemplateView):
+    template_name = 'orders/success.html'
+    title = 'Store - Success'
+
+
+class CancelTemplateView(TemplateView):
+    template_name = 'orders/cancel.html'
+
+
+class OrderListView(TitleMixin, ListView):
+    template_name = 'orders/orders.html'
+    title = 'Store - Orders'
+    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        queryset = super(OrderListView, self).get_queryset()
+        return queryset.filter(initiator=self.request.user)
+
+
+class OrderDetailView(DetailView):
+    template_name = 'orders/order.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderDetailView, self).get_context_data(**kwargs)
+        context['title'] = f'Store - Order #{self.object.id}'
+        return context
 
 
 class OrderCreateView(TitleMixin, CreateView):
@@ -11,9 +53,54 @@ class OrderCreateView(TitleMixin, CreateView):
     success_url = reverse_lazy('orders:order_create')
     title = 'Store - Order Create'
 
+    def post(self, request, *args, **kwargs):
+        super(OrderCreateView, self).post(self, request, *args, **kwargs)
+        baskets = Basket.objects.filter(user=self.request.user)
+
+        checkout_session = stripe.checkout.Session.create(
+            line_items=baskets.stripe_products(),
+            mode='payment',
+            metadata={'order_id': self.object.id},
+            success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
+            cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_cancelled')),
+        )
+        return HttpResponseRedirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
+
     def form_valid(self, form):
         form.instance.initiator = self.request.user
         return super(OrderCreateView, self).form_valid(form)
 
 
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        # Fulfill the purchase...
+        fulfill_order(session)
+
+        # Passed signature verification
+        return HttpResponse(status=200)
+
+
+def fulfill_order(session):
+    order_id = int(session.metadata.order_id)
+    order = Order.objects.get(id=order_id)
+    order.update_after_payment()
 
